affc544d04abaa8e5a98240fc1fc0cdf82173bfa
Vinay046 <vinayvp.98@gmail.com>
2024-03-02 15:00:04 +0100

    Second Commit- Fix package name

--- launch/talker_listener.launch.py
+++ launch/talker_listener.launch.py
@@ -4,12 +4,12 @@ import launch_ros.actions
 def generate_launch_description():
     return launch.LaunchDescription([
         launch_ros.actions.Node(
-            package='ros-test',
+            package='ros2-test',
             executable='listener',
             name='listener'
         ),
         launch_ros.actions.Node(
-            package='ros-test',
+            package='ros2-test',
             executable='talker',
             name='talker'
         ),
